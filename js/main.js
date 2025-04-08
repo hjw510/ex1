f3299d656403ff2a8d1584224c8205127fe0b94e
hjw <mabb55@naver.com>
2025-04-08 12:01:45 +0900

    correct2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
+const langBtn = document.querySelector('.lang_wrap button')
+const langList = document.querySelector('.lang_wrap ul')
+const langIco = document.querySelector('.lang_wrap button span')
 
+langBtn.addEventListener('click', function() {
+  langList.classList.toggle('active')
+  langIco.classList.toggle('active')
+})
 
 const swiper1 = new Swiper('#all_product_content', {
 		autoplay: true,
